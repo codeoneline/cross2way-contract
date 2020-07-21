a44e1bcabc0c4fff8d5ec97562003bf473574001
codeoneline <mutouji1@163.com>
2020-07-21 17:41:46 +0800

    add storeman group admin event

--- contracts/StoremanGroupAdmin.sol
+++ contracts/StoremanGroupAdmin.sol
@@ -2,6 +2,16 @@ pragma solidity 0.4.26;
 pragma experimental ABIEncoderV2;
 
 contract StoremanGroupAdmin {
+  enum GroupStatus {none, initial, curveSeted, failed, selected, ready, unregistered, dismissed}
+
+  event registerStartEvent(bytes32 indexed groupId, uint workStart,uint workDuration, uint registerDuration, bytes32 indexed preGroupId);
+  event StoremanGroupUnregisterEvent(bytes32 indexed groupId);
+
+  event StoremanGroupDismissedLogger(bytes tokenOrigAccount, bytes storemanGroup, uint dismissTime);
+  event storemanTransferEvent(bytes32 indexed groupId, bytes32 indexed preGroupId, address[] wkAddrs);
+
+  event storemanGroupContributeEvent(address indexed sender, uint indexed value);
+
   struct StoremanGroupConfig {
     uint8   status;
     uint    chain1;
@@ -14,10 +24,14 @@ contract StoremanGroupAdmin {
     uint    endTime;
   }
 
-  int total;
+  int public total;
   mapping(bytes32 => StoremanGroupConfig) public mapConfig;
   mapping(bytes32 => uint) public mapDeposit;
   mapping(bytes32 => uint) public mapFee;
+
+  mapping(bytes => mapping(bytes => bytes32)) storemanGroupMap;
+
+  address[] public oldAddr;
 
   function smgTransfer(bytes32 smgID) public payable
   {
@@ -66,5 +80,43 @@ contract StoremanGroupAdmin {
     endTime = mapConfig[id].endTime;
 
     deposit = mapDeposit[id];
+  }
+
+  // register event
+  function registerStart(bytes32 groupId,
+    uint workStart, uint workDuration, uint registerDuration,  bytes32 preGroupId)
+    // uint workStart,uint workDuration, uint registerDuration,  bytes32 preGroupId,  address[] wkAddrs, address[] senders)
+    external
+  {
+    mapConfig[groupId].status = uint8(GroupStatus.initial);
+    // mapConfig[groupId].startTime = workStart;
+    // mapConfig[groupId].endTime = workStart + workDuration;
+
+    // mapDeposit[groupId].deposit = deposit;
+
+    emit registerStartEvent(groupId, workStart, workDuration, registerDuration, preGroupId);
+    if(preGroupId != bytes32(0x00)) {
+      emit storemanTransferEvent(groupId, preGroupId, oldAddr);
+    }
+  }
+
+  function storemanGroupUnregister(bytes32 groupId)
+    external
+  {
+    mapConfig[groupId].status = uint8(GroupStatus.unregistered);
+    emit StoremanGroupUnregisterEvent(groupId);
+  }
+
+  function storemanGroupDismiss(bytes tokenOrigAccount, bytes storemanGroup, bytes32 groupId)
+  // function storemanGroupDismiss(bytes tokenOrigAccount, bytes storemanGroup, bytes32 )
+    external
+  {
+    mapConfig[groupId].status = uint8(GroupStatus.dismissed);
+    emit StoremanGroupDismissedLogger(tokenOrigAccount, storemanGroup, now);
+  }
+
+  function contribute() public payable {
+    emit storemanGroupContributeEvent(msg.sender, msg.value);
+    return;
   }
 }
